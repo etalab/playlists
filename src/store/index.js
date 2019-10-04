2db624f83536782d556af595cb4dcd879b5ec2bc
taniki <tk@deveha.com>
2019-10-04 16:46:33 +0200

    refactoring

---
+++ src/store/index.js
@@ -0,0 +1,65 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import folders from '~/store/modules/folders'
+
+Vue.use(Vuex)
+
+const state = function(){
+  return {
+    user: {
+      loggedIn: false,
+      token: '',
+      data: {}
+    }
+  }
+}
+
+const mutations = {
+  setLoggedIn (state) {
+    state.user.loggedIn = true
+  },
+  unsetLoggedIn (state) {
+    state.user.loggedIn = false
+  },
+  setToken (state, token) {
+    state.user.token = token
+  },
+  setUserData (state, data) {
+    state.user.data = data
+  },
+  // restore store.state from localStorage
+  initialiseStore (state) {
+    if (localStorage.getItem('store')) {
+      this.replaceState(
+        Object.assign(state, JSON.parse(localStorage.getItem('store')))
+      )
+    }
+  }
+}
+
+const actions = {
+  login ({ commit }, token) {
+    commit('setLoggedIn')
+    commit('setToken', token)
+  },
+  fillUserData ({ commit }, data) {
+    commit('setUserData', data)
+  },
+  logout ({ commit }) {
+    commit('unsetLoggedIn')
+    commit('setToken', '')
+    commit('setUserData', {})
+    commit('folders/setFolders', [])
+  }
+}
+
+export default new Vuex.Store({
+    strict: true,
+    modules:{
+        folders: folders
+    },
+    state: state,
+    mutations: mutations,
+    actions: actions
+})
---
+++ src/store/modules/folders.js
@@ -0,0 +1,48 @@
+import Api from '~/services/Api.js'
+
+const $api = new Api()
+
+const module = {
+    namespaced: true,
+    state(){
+        return {
+            cache: []
+        }
+    },
+    mutations: {
+        setFolders(state, data){
+            state.cache = data
+        }
+    },
+    actions: {
+        fetchMe: async function({ commit }){
+            let folders = []
+
+            const res = await $api.get('me/datasets')
+
+            for (let k in res.data) {
+                const dataset = res.data[k]
+                if ("playlist" in dataset.extras){
+                    folders.push(dataset)
+                }
+            }
+
+            if (folders.length == 0){
+                const info = {
+                    title: "Mes playlists",
+                    description: "mes belles playlists",
+                    private: true,
+                    extras: {
+                        playlist: true
+                    }
+                }
+
+                $api.post('datasets', info)
+            }
+
+            commit("setFolders", folders)
+        }
+    }
+}
+
+export default module
